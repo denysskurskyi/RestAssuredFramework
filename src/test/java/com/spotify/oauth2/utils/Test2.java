217604ab5b81c42d37414969995fa37215e59de7
Denys Skurskyi <denys.skurskyi@gmail.com>
2022-05-11 13:11:26 +0300

    Commit everything in master

--- src/test/java/com/spotify/oauth2/utils/Test2.java
+++ src/test/java/com/spotify/oauth2/utils/Test2.java
@@ -4,9 +4,15 @@ public class Test2 {
 
     public void printTestMasterMessage() {
         System.out.println("Test Master class");
+        System.out.println("Test Master class");
+        System.out.println("Test Master class");
     }
 
     public void MasterPrint() {
         System.out.println("MasterPrint");
+    }
+
+    public String MasterPrint(String str) {
+        return str;
     }
 }
---
+++ src/test/java/com/spotify/oauth2/utils/Test3.java
@@ -0,0 +1,6 @@
+package com.spotify.oauth2.utils;
+
+public class Test3 {
+
+    private String test3;
+}
